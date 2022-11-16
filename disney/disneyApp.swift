f2782c89c130a779c5d1309c150c5386b533ea4f
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-11-16 17:27:50 -0500

    Initial Commit

---
+++ disney/disneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  disneyApp.swift
+//  disney
+//
+//  Created by AlecNipp on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct disneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
